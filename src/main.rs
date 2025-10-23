0a9e53f89a3a3c56ec16ad2631e9806f21429f9f
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-10-23 15:13:29 +0530

    Initialize Rust project structure and add main functionality

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Hello World
+    println!("Hello, world!");
+
+    // Variables
+    let x: i8 = 10;
+    let y: f32 = 10.0;
+    println!("x = {}", x);
+    println!("y = {}", y)
+}
